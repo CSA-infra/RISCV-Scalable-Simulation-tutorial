95f510d2d1546350824892caf561a5afe600c8bd
Debjyoti Bhattacharjee <debjyoti.bhattacharjee@imec.be>
2024-12-06 14:31:49 +0100

    [docs] remove emoji package

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme',
-              'sphinxemoji.sphinxemoji']
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
